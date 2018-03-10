c6073f3227b9d4cf4e4e419a7ba1186869e108c5
philguskov <37250440+philguskov@users.noreply.github.com>
2018-03-10 23:30:51 +0300

    Update dice.py

--- main/dice.py
+++ main/dice.py
@@ -1,7 +1,7 @@
 from random import randint 
 from telegram import *
 
-updater = Updater(token="TOKEN")
+updater = Updater(token="554767023:AAFSn7jxojOoNeoN8-FfGHwAg2Wz7UmOxF4")
 dispatcher = updater.dispatcher
 
 # D4, D6, D8, D10, D12, D20
